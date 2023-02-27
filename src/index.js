e8b170cc8b63c017e7570a4e331c1a55422b8790
bacugniano <bakuntsev@gmail.com>
2023-02-28 00:50:16 +0300

    feat: test complite

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = [],
+      newMatrix = [];
+
+  if (!matrix) {
+    return newArr;
+  } else {
+    newMatrix = matrix.map((elem, index) => {
+      if (index % 2 !== 0) {
+        return elem.reverse()
+      } else return elem;
+    })
+
+    newMatrix.forEach((elem) => {
+      elem.forEach(el => {
+        newArr.push(el)
+      })
+    })
+  }
+
+  return newArr
 }
